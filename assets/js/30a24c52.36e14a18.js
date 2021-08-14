2a38148bae98593177854a1e00732fc1c0edeb74
cloudmex-alan <alanestrada@cloudmex.io>
2021-08-14 09:46:44 -0600

    Deploy website - based on 30c072b6fb905fe071d9322504c9b097c91b455f

--- assets/js/30a24c52.36e14a18.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkcloudmex_academy=self.webpackChunkcloudmex_academy||[]).push([[453],{516:function(e){"use strict";e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/blog/tags/hello"}')}}]);
---
+++ assets/js/814f3328.6f2476a1.js
@@ -0,0 +1,1 @@
+(self.webpackChunkcloudmex_academy=self.webpackChunkcloudmex_academy||[]).push([[535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
